539178975bec0a5c875e65fa8e07141ec65ac27b
Joren-Thijs <joren.thijs@hotmail.com>
2021-08-27 00:27:08 +0200

    added stuff

---
+++ app/src/main/java/com/shadowcorp/firstapp/NewDiceActivity.java
@@ -0,0 +1,14 @@
+package com.shadowcorp.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewDiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_dice);
+    }
+}
